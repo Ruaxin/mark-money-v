a6d2f2f1dc3895c989f365a8a91d0701f020afa1
ruaxin <qy.xin@foxmail.com>
2020-01-27 22:07:45 +0800

    更改viewport

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -23,22 +23,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  nav{
+  nav {
     display: flex;
     flex-direction: row;
-    box-shadow: 0 0 3px rgba(0,0,0,0.25);
-    font-size:12px;
-    > .item{
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+    font-size: 12px;
+    color: rgb(153, 153, 153);
+
+    > .item {
       padding: 2px 0;
       width: 33.333333%;
       display: flex;
       justify-content: center;
       align-items: center;
       flex-direction: column;
-      .icon{
+
+      .icon {
         width: 32px;
         height: 32px;
       }
+    }
+
+    > .item.selected {
+      color: rgb(51,51,51);
     }
   }
 </style>
