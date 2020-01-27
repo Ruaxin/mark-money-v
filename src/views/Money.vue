e762dce3578ee29e8306555196a1be4ec51908f0
ruaxin <qy.xin@foxmail.com>
2020-01-27 18:26:36 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
+  <div class="nav-wrapper">
+    <div class="content">
     Money
+    </div>
     <Nav/>
   </div>
 </template>
@@ -12,5 +14,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper {
+    border: 1px solid blue;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    flex-grow: 1;
+    overflow: auto;
+    border: 1px solid black;
+  }
 </style>
