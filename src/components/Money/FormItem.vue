8b34ecf8c8a4dac8d97272be7d8b06ec4516c51a
ruaxin <qy.xin@foxmail.com>
2020-02-10 22:14:47 +0800

    完成CSS，提取了几个通用组件

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -1,22 +1,21 @@
 <template>
   <div>
-    <label class="notes">
+    <label class="formItem">
       <span class="name">{{this.fieldName}}</span>
-      <input type="text" v-model="value" :placeholder="this.placeholder">
+      <input type="text" v-model="value" :placeholder="placeholder">
     </label>
   </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue';
-  import {Component, Prop, Watch} from 'vue-property-decorator';
+  import {Component, Prop} from 'vue-property-decorator';
   @Component
-  export default class Notes extends Vue {
+  export default class FormItem extends Vue {
     value = '';
     @Prop({required:true})fieldName!:string;
     @Prop()placeholder?:string;
 
-    @Watch('value')
     onValueChanged(value:string){
       this.$emit('update:value',value);
     }
@@ -24,9 +23,8 @@
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+  .formItem {
     font-size: 14px;
-    background: #f5f5f5;
     padding-left: 16px;
     display: flex;
     align-items: center;
@@ -36,7 +34,7 @@
     }
 
     > input {
-      height: 64px;
+      height: 40px;
       flex-grow: 1;
       background: transparent;
       border: none;
