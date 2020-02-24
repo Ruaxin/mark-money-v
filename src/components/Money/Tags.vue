6cdb7616f95eaf400d3bc6997ae8562eb5382122
ruaxin <qy.xin@foxmail.com>
2020-02-24 08:28:14 +0800

    tags使用vuex

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -19,13 +19,16 @@ import {Component} from 'vue-property-decorator';
 @Component({
   computed:{
     tagList(){
-      return [];
+      return this.$store.state.tagList;
     }
   }
 })
 export default class Tags extends Vue {
   selectedTags: string[] = [];
 
+  created(){
+    this.$store.commit('fetchTags');
+  }
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
@@ -41,7 +44,7 @@ export default class Tags extends Vue {
     if (!name) {
       return window.alert('标签名不能为空');
     }
-    //store.createTag(name);
+    this.$store.commit('createTag',name);
   }
 }
 </script>
